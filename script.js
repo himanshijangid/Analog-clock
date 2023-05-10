c90ac89df3ba57b8aefea434085592df77c320f1
Himanshi Jangid <himanshijangid444@gmail.com>
2023-05-10 13:40:23 +0530

    iuij

--- script.js
+++ script.js
@@ -1,25 +1,13 @@
-const sec = document.querySelector('.second-hand')
-const min = document.querySelector('.min-hand')
-const hr = document.querySelector('.hour-hand')
-const h1 = document.querySelector('h2')
-
-function setDate() {
-    const now = new Date();
-    const seconds = now.getSeconds();
-    const minute = now.getMinutes();
-    const hour = now.getHours();
-    const digiHour = hour>12?(hour-12):hour
-    const midi = hour<=12? 'am':'pm'
-
-    const secDeg = (seconds/60)*360+90
-    const minDeg = ((minute/60)*360)+((seconds/60)*6)+90
-    const hrDeg = (hour*30)+((minute/60)*30)+90
-
-    sec.style.transform = `rotate(${secDeg}deg)`
-    min.style.transform = `rotate(${minDeg}deg)`
-    hr.style.transform = `rotate(${hrDeg}deg)`
-    h1.innerHTML = `${digiHour}:${minute}:${seconds} ${midi}`
-}
-
-
-setInterval(setDate,1000);
+setInterval(() => {
+    d = new Date(); //object of date()
+    hr = d.getHours();
+    min = d.getMinutes();
+    sec = d.getSeconds();
+    hr_rotation = 30 * hr + min / 2; //converting current time
+    min_rotation = 6 * min;
+    sec_rotation = 6 * sec;
+    
+    hour.style.transform = `rotate(${hr_rotation}deg)`;
+    minute.style.transform = `rotate(${min_rotation}deg)`;
+    second.style.transform = `rotate(${sec_rotation}deg)`;
+    }, 1000);
